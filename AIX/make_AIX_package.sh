8a90bb776e6d2fd4f1a9c60776919537214cda5e
Andreas Leibl <andreas@leibl.co.uk>
2014-05-22 13:19:14 +0100

    Some cleanup to AIX packaging.

--- AIX/make_AIX_package.sh
+++ AIX/make_AIX_package.sh
@@ -7,13 +7,9 @@ mkdir -p usr/bin
 cp COPYRIGHT opt/CertNanny/COPYRIGHT.sscep
 cp sscep_static opt/CertNanny/bin
 cp sscep_dyn opt/CertNanny/bin
-cp AIX/sscep.pre-install.sh opt/CertNanny/inst
-cp AIX/sscep.pre-deinstall.sh opt/CertNanny/inst
-# compute the version number (VRML)
+# compute the version number (VRMF)
 version=$(head -n 1 VERSION)
 version="$version.0"
-# provide the override inventory file to file ownership and permissions
-cp AIX/override_inventory /tmp
 # create the template and building the package
 sed "s/VERSIONINFO/$version/" < AIX/lpp_template.in | sed "s#__PACKAGINGDIR__#$PWD#" > AIX/lpp_template
 # make sure we can execute mkinstallp ... if not, try sudo
